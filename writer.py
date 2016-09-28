773c85b76dc8e9c8262d5fedf30ee1249adae65b
David Loukidelis <sdloukid@uwaterloo.ca>
2016-09-27 21:15:05 -0400

    MVP. Adds sentences from sentence bank and signs name at end.

--- writer.py
+++ writer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import sys
+import string
+
+LINE_LENGTH = 70
 
 input = []
 if (len(sys.argv) > 1):
@@ -7,6 +10,25 @@ if (len(sys.argv) > 1):
 else:
   with open('input.txt') as f:
     for line in f:
-      input = input + line.split()
+      for word in line.split():
+        word = word.translate(string.maketrans("",""), string.punctuation)
+        input.append(word.lower())
 
-print input
+words = {}
+with open('words.txt') as f:
+  for line in f:
+    pair = line.split('\\')
+    words[pair[0]] = pair[1]
+
+letter = ""
+line = ""
+for word in input:
+  if word in words:
+    line += words[word]
+    
+    if len(line) > LINE_LENGTH:
+      letter += line
+      letter += "\n"
+
+letter += "Sincerely,\nDavid Loukidelis\n"
+print letter
