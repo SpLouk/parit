4abe0471517a528f5a9001dda89a2b1bcd099114
David Loukidelis <david@loukidelis.com>
2017-09-23 12:50:59 -0400

    Add print_postings function to scraper.py
    print_postings will print the "Required skills" section of each
    job that is returned by the given search term.

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ import requests
 
 LOGIN_URL = 'https://cas.uwaterloo.ca/cas/login?service=https://waterlooworks.uwaterloo.ca/waterloo.htm'
 DASH_URL = 'https://waterlooworks.uwaterloo.ca/myAccount/dashboard.htm'
+POSTINGS_URL = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm"
 
 def login_cas_waterloo_works(credentials_file):
   credentials = json.loads(open(credentials_file).read())
@@ -33,6 +34,30 @@ def print_messages(credentials_file):
   # subjects
   messages = tree.xpath('//table/tbody/tr/td[7]/text()')
   print messages
+
+def print_postings(credentials_file, search_term):
+  session = login_cas_waterloo_works(credentials_file)
+  payload = {
+    'action': '_-_-MluhXCvR0n6jA3vbq7A87AJL7KvOHm7xG2EUvL3M9hTt--LmoKJexZQTg-Hc1tVOY51oqx3B0z0B3XfEwyDWpUF7tglFyshhqUlF8289hLmdQbmUc1EoqRENQ8X4t2kUlbPiyEFZ8M3wFKbiTxpVPfKD9t9X5tanfajsPtE',
+    'filter': 'all',
+    'widget1Keyword': search_term,
+    'filter2': 'exact'
+  }
+  page = session.post(POSTINGS_URL, data=payload)
+  tree = html.fromstring(page.content)
+  posting_ids = tree.xpath('//table[@id="postingsTable"]/tbody/tr/td[3]/text()')
+
+  for posting_id in posting_ids:
+    payload = {
+      'action': '_-_-CZAJhGzDi93Ir5yebC9X2QzZQPKmuf0qAY5WqOitfQhUFJT3pAAHQ8WR_qCCCR0IF9vJt6KnkqRkOUMIbOwOiS9iCc2vioHN55ytvgjL1NnXn2kskxVw3GE6CalJ6PidQrNecI9flFNxn6E3C6mMzw4czSk8ygpnu7FlbiWT_Q',
+      'postingId': posting_id
+    }
+    page = session.post(POSTINGS_URL, data=payload)
+    tree = html.fromstring(page.content)
+    posting = tree.xpath('//div[@id="postingDiv"]/div/div/table/tbody/tr[17]/td[2]/text()')
+    print '--------------------------{0}--------------------------'.format(posting_id)
+    for item in posting:
+      print item.strip()
 
 if __name__ == '__main__':
   print_messages()
