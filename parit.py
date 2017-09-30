0f1bbd1df5746aae412cb71c9f124147ffa72227
David Loukidelis <david@loukidelis.com>
2017-09-30 10:36:11 -0400

    Change indentation from 2 to 4 spaces
    According to [PEP-8](https://www.python.org/dev/peps/pep-0008/)
    indentation should be 4 spaces.

--- parit.py
+++ parit.py
@@ -10,117 +10,116 @@ import yaml
 
 # Takes a sentence in the form of a string and splits it into its words with all punctuation removed.
 def process_input(line):
-  line = line.translate(string.maketrans("/"," "), string.punctuation.replace('+', ''))
-  return line.split()
+    line = line.translate(string.maketrans("/"," "), string.punctuation.replace('+', ''))
+    return line.split()
 
 class Letter(object):
-  def __init__(self, config, sentences, posting):
-    self.config = config
-    self.sentences = sentences
-    self.posting = posting
-    self.body = []
+    def __init__(self, config, sentences, posting):
+        self.config = config
+        self.sentences = sentences
+        self.posting = posting
+        self.body = []
 
-  def employer_address(self):
-    return '{0}{1}{2}{3}{4}{5}'.format(
-      ('{0}\n'.format(self.posting.get('organization')) if self.posting.get('organization') else ''),
-      ('{0}\n'.format(self.posting.get('address_one')) if self.posting.get('address_one') else ''),
-      ('{0}\n'.format(self.posting.get('address_two')) if self.posting.get('address_two') else ''),
-      ('{0} '.format(self.posting.get('city')) if self.posting.get('city') else ''),
-      ('{0} '.format(self.posting.get('province')) if self.posting.get('province') else ''),
-      (self.posting.get('postal_code') if self.posting.get('postal_code') else ''))
+    def employer_address(self):
+        return '{0}{1}{2}{3}{4}{5}'.format(
+                ('{0}\n'.format(self.posting.get('organization')) if self.posting.get('organization') else ''),
+                ('{0}\n'.format(self.posting.get('address_one')) if self.posting.get('address_one') else ''),
+                ('{0}\n'.format(self.posting.get('address_two')) if self.posting.get('address_two') else ''),
+                ('{0} '.format(self.posting.get('city')) if self.posting.get('city') else ''),
+                ('{0} '.format(self.posting.get('province')) if self.posting.get('province') else ''),
+                (self.posting.get('postal_code') if self.posting.get('postal_code') else ''))
 
-  def return_address(self):
-    return '{0}{1}{2} {3} {4}'.format(
-      ('{0}\\\\'.format(self.config.get('address_one')) if self.config.get('address_one') else ''),
-      ('{0}\\\\'.format(self.config.get('address_two')) if self.config.get('address_two') else ''),
-      self.config.get('city', ''),
-      self.config.get('province', ''),
-      self.config.get('postal_code', ''))
+    def return_address(self):
+        return '{0}{1}{2} {3} {4}'.format(
+                ('{0}\\\\'.format(self.config.get('address_one')) if self.config.get('address_one') else ''),
+                ('{0}\\\\'.format(self.config.get('address_two')) if self.config.get('address_two') else ''),
+                self.config.get('city', ''),
+                self.config.get('province', ''),
+                self.config.get('postal_code', ''))
 
-  def select_sentence(self, word):
-    sentence = self.sentences[word]
-    if isinstance(sentence, basestring):
-      return sentence
-    else:
-      index = int(random.uniform(0, len(sentence)))
-      return sentence[index]
-
-  def write_body(self):
-    self.body = ['']
-    words = {}
-    for word in process_input(self.posting.get('job_responsibilities') + self.posting.get('required_skills')):
-      if word.lower() in self.sentences:
-        if word in words:
-          words[word] += 1
+    def select_sentence(self, word):
+        sentence = self.sentences[word]
+        if isinstance(sentence, basestring):
+            return sentence
         else:
-          words[word] = 1
+            index = int(random.uniform(0, len(sentence)))
+            return sentence[index]
 
-    for word in sorted(words, key=words.get, reverse=True):
-      self.body.append(self.select_sentence(word.lower()))
+    def write_body(self):
+        self.body = ['']
+        words = {}
+        for word in process_input(self.posting.get('job_responsibilities') + self.posting.get('required_skills')):
+            if word.lower() in self.sentences:
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
 
-    return ' '.join(self.body)
+        for word in sorted(words, key=words.get, reverse=True):
+            self.body.append(self.select_sentence(word.lower()))
 
-  def write(self):
+        return ' '.join(self.body)
 
+    def write(self):
+        opening = self.config['opening'].replace('$organization', self.posting.get('organization'))
+        closing = self.config['closing'].replace('$organization', self.posting.get('organization'))
+        output = Document(documentclass='letter')
+        output.preamble.append(Command('signature', self.config['sender']))
+        output.preamble.append(Command('address', NoEscape(self.return_address())))
+        output.preamble.append(Command('date', NoEscape(r'\today')))
+        output.append(Command('begin', ['letter', self.employer_address()]))
+        output.append(Command('opening', 'Dear Hiring Manager,'))
+        output.append(opening + self.write_body())
+        output.append(NewLine())
+        output.append(NewLine())
+        output.append(closing)
+        output.append(Command('closing', 'Sincerely,'))
+        output.append(Command('end', 'letter'))
 
-    opening = self.config['opening'].replace('$organization', self.posting.get('organization'))
-    closing = self.config['closing'].replace('$organization', self.posting.get('organization'))
-    output = Document(documentclass='letter')
-    output.preamble.append(Command('signature', self.config['sender']))
-    output.preamble.append(Command('address', NoEscape(self.return_address())))
-    output.preamble.append(Command('date', NoEscape(r'\today')))
-    output.append(Command('begin', ['letter', self.employer_address()]))
-    output.append(Command('opening', 'Dear Hiring Manager,'))
-    output.append(opening + self.write_body())
-    output.append(NewLine())
-    output.append(NewLine())
-    output.append(closing)
-    output.append(Command('closing', 'Sincerely,'))
-    output.append(Command('end', 'letter'))
-
-    filename = '{0}_{1}'.format(self.posting['organization'], ' '.join(process_input(self.posting['job_title']))).replace(' ', '_').lower()
-    output.generate_pdf(filename)
-    output.generate_tex(filename)
+        filename = '{0}_{1}'.format(self.posting['organization'], ' '.join(process_input(self.posting['job_title']))).replace(' ', '_').lower()
+        output.generate_pdf(filename)
+        output.generate_tex(filename)
 
 def main():
-  parser = argparse.ArgumentParser(description='This script will write you a cover letter')
-  parser.add_argument('-c', '--config', help=('Directory containing config file (config.yml), '
-    'sentences file (sentences.yml), and credentials file (credentials.yml). You must have these '
-    'files for the program to run properly. (default: ~/.config/parit/)'),
-    default='~/.config/parit/')
-  parser.add_argument('-p', '--posting-id', help='Waterloo Works job posting Id')
-  parser.add_argument('-t', '--term', help='Search term for Waterloo Works')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='This script will write you a cover letter')
+    parser.add_argument('-c', '--config', help=('Directory containing config file (config.yml), '
+        'sentences file (sentences.yml), and credentials file (credentials.yml). You must have these '
+        'files for the program to run properly. (default: ~/.config/parit/)'),
+        default='~/.config/parit/')
+    parser.add_argument('-p', '--posting-id', help='Waterloo Works job posting Id')
+    parser.add_argument('-t', '--term', help='Search term for Waterloo Works')
+    args = parser.parse_args()
 
-  config_path = os.path.expanduser(args.config)
-  try:
-    with open('{0}/config.yml'.format(config_path)) as f:
-      config = yaml.load(f)
-  except IOError:
-    config = {}
+    config_path = os.path.expanduser(args.config)
+    try:
+        with open('{0}/config.yml'.format(config_path)) as f:
+            config = yaml.load(f)
+    except IOError:
+        config = {}
 
-  try:
-    with open('{0}/sentences.yml'.format(config_path)) as f:
-      sentences = yaml.load(f)
-  except IOError:
-    sys.exit("You must provide a sentences file")
+    try:
+        with open('{0}/sentences.yml'.format(config_path)) as f:
+            sentences = yaml.load(f)
+    except IOError:
+        sys.exit("You must provide a sentences file")
 
-  sender = config.get('sender') or sys.exit('You must provide a sender name in either your config or an arg')
+    sender = config.get('sender') or sys.exit('You must provide a sender name in either your config or an arg')
 
-  try:
-    with open('{0}/credentials.yml'.format(config_path)) as f:
-      credentials = yaml.load(f)
-  except IOError:
-    sys.exit("You must provide a credentials file")
+    try:
+        with open('{0}/credentials.yml'.format(config_path)) as f:
+            credentials = yaml.load(f)
+    except IOError:
+        sys.exit("You must provide a credentials file")
 
-  session = scraper.login_cas_waterloo_works(credentials.get('username'), credentials.get('password'))
-  postings = [scraper.get_posting(session, args.posting_id)] if args.posting_id else scraper.get_postings(session, args.term)
+    session = scraper.login_cas_waterloo_works(credentials.get('username'), credentials.get('password'))
 
-  for posting in postings:
-    print 'Generating cover letter for {0}'.format(posting['organization'])
-    letter = Letter(config, sentences, posting)
-    letter.write()
-    print 'Done!'
+    postings = [scraper.get_posting(session, args.posting_id)] if args.posting_id else scraper.get_postings(session, args.term)
+
+    for posting in postings:
+        print 'Generating cover letter for {0}'.format(posting['organization'])
+        letter = Letter(config, sentences, posting)
+        letter.write()
+        print 'Done!'
 
 if __name__ == "__main__":
-  main()
+    main()
--- scraper.py
+++ scraper.py
@@ -10,68 +10,67 @@ LOGIN_URL = 'https://cas.uwaterloo.ca/cas/login?service=https://waterlooworks.uw
 POSTINGS_URL = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm"
 
 def login_cas_waterloo_works(username, password):
-  payload = {
-    'username': username,
-    'password': password,
-    'lt': 'e1s1',
-    '_eventId': 'submit'
-  }
-  session = requests.Session()
-  session.get(LOGIN_URL) # sets cookies in session
-  session.post(LOGIN_URL, data=payload)
-  return session
+    payload = {
+        'username': username,
+        'password': password,
+        'lt': 'e1s1',
+        '_eventId': 'submit'
+    }
+    session = requests.Session()
+    session.get(LOGIN_URL) # sets cookies in session
+    session.post(LOGIN_URL, data=payload)
+    return session
 
 def make_key(key):
-  key = key.translate(string.maketrans(" ","_"), string.punctuation).lower()
-  if ('country' in key):
-    return 'country'
-  elif ('province' in key):
-    return 'province'
-  elif ('city' in key):
-    return 'city'
-  elif ('address' in key):
-    if ('one' in key):
-      return 'address_one'
+    key = key.translate(string.maketrans(" ","_"), string.punctuation).lower()
+    if ('country' in key):
+        return 'country'
+    elif ('province' in key):
+        return 'province'
+    elif ('city' in key):
+        return 'city'
+    elif ('address' in key):
+        if ('one' in key):
+            return 'address_one'
+        else:
+            return 'address_two'
+    elif ('postal_code' in key):
+        return 'postal_code'
     else:
-      return 'address_two'
-  elif ('postal_code' in key):
-    return 'postal_code'
-  else:
-    return key
+        return key
 
 def parse_posting(posting):
-  # there are malformed unicode characters in the HTML of this page, so we need to process it again and ignore errors
-  page = unicode(posting.content, errors='ignore')
+    # there are malformed unicode characters in the HTML of this page, so we need to process it again and ignore errors
+    page = unicode(posting.content, errors='ignore')
 
-  tree = html.fromstring(page)
-  posting = {}
-  for row in tree.xpath('//div[@id="postingDiv"]/div/div/table/tbody/tr'):
-    key = ''.join(row.xpath('td[1]/descendant::text()')).strip()
-    val = ''.join(row.xpath('td[2]/descendant::text()')).strip()
-    posting[make_key(key)] = val
-
-  return posting
+    tree = html.fromstring(page)
+    posting = {}
+    for row in tree.xpath('//div[@id="postingDiv"]/div/div/table/tbody/tr'):
+        key = ''.join(row.xpath('td[1]/descendant::text()')).strip()
+        val = ''.join(row.xpath('td[2]/descendant::text()')).strip()
+        posting[make_key(key)] = val
+    return posting
 
 
 def get_posting(session, posting_id):
-  payload = {
-    'action': '_-_-CZAJhGzDi93Ir5yebC9X2QzZQPKmuf0qAY5WqOitfQhUFJT3pAAHQ8WR_qCCCR0IF9vJt6KnkqRkOUMIbOwOiS9iCc2vioHN55ytvgjL1NnXn2kskxVw3GE6CalJ6PidQrNecI9flFNxn6E3C6mMzw4czSk8ygpnu7FlbiWT_Q',
-    'postingId': posting_id
-  }
-  return parse_posting(session.post(POSTINGS_URL, data=payload))
+    payload = {
+        'action': '_-_-CZAJhGzDi93Ir5yebC9X2QzZQPKmuf0qAY5WqOitfQhUFJT3pAAHQ8WR_qCCCR0IF9vJt6KnkqRkOUMIbOwOiS9iCc2vioHN55ytvgjL1NnXn2kskxVw3GE6CalJ6PidQrNecI9flFNxn6E3C6mMzw4czSk8ygpnu7FlbiWT_Q',
+        'postingId': posting_id
+    }
+    return parse_posting(session.post(POSTINGS_URL, data=payload))
 
 def get_postings(session, search_term):
-  payload = {
-    'action': '_-_-MluhXCvR0n6jA3vbq7A87AJL7KvOHm7xG2EUvL3M9hTt--LmoKJexZQTg-Hc1tVOY51oqx3B0z0B3XfEwyDWpUF7tglFyshhqUlF8289hLmdQbmUc1EoqRENQ8X4t2kUlbPiyEFZ8M3wFKbiTxpVPfKD9t9X5tanfajsPtE',
-    'filter': 'all',
-    'widget1Keyword': search_term,
-    'filter2': 'exact'
-  }
-  page = session.post(POSTINGS_URL, data=payload)
-  tree = html.fromstring(page.content)
-  posting_ids = tree.xpath('//table[@id="postingsTable"]/tbody/tr/td[3]/text()')
+    payload = {
+        'action': '_-_-MluhXCvR0n6jA3vbq7A87AJL7KvOHm7xG2EUvL3M9hTt--LmoKJexZQTg-Hc1tVOY51oqx3B0z0B3XfEwyDWpUF7tglFyshhqUlF8289hLmdQbmUc1EoqRENQ8X4t2kUlbPiyEFZ8M3wFKbiTxpVPfKD9t9X5tanfajsPtE',
+        'filter': 'all',
+        'widget1Keyword': search_term,
+        'filter2': 'exact'
+    }
+    page = session.post(POSTINGS_URL, data=payload)
+    tree = html.fromstring(page.content)
+    posting_ids = tree.xpath('//table[@id="postingsTable"]/tbody/tr/td[3]/text()')
 
-  postings = []
-  for posting_id in posting_ids:
-    postings.append(get_posting(session, posting_id))
-  return postings
+    postings = []
+    for posting_id in posting_ids:
+        postings.append(get_posting(session, posting_id))
+    return postings
